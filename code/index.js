3bd42d8658ac110a7d53216eda95ad8f357a422a
Giulia <giuliac@outlook.com>
2023-03-10 12:00:28 +0200

    item count and add fixed now working

--- code/index.js
+++ code/index.js
@@ -1,132 +1,112 @@
 // selecting items 
 const alert = document.querySelector(".alert"),
-	list = document.querySelector(".list"),
-	content = document.querySelector(".content"),
-	checkbox = document.querySelectorAll("input[type=checkbox]"),
-	newTodo = document.getElementById("new-todo"),
-	filterAll = document.getElementById("all"),
-	filterActive = document.getElementById("active"),
-	filterCompleted = document.getElementById("completed"),
-	clearCompleted = document.querySelector(".clear-completed");
+  content = document.querySelector(".content"),
+  checkbox = document.querySelectorAll("input[type=checkbox]"),
+  newTodo = document.getElementById("new-todo"),
+  filterAll = document.getElementById("all"),
+  filterActive = document.getElementById("active"),
+  filterCompleted = document.getElementById("completed"),
+  clearCompleted = document.querySelector(".clear-completed");
 const input = document.querySelector('input')
+const itemsLeft = document.querySelector(".items-left")
 
-//event listeners
-input.addEventListener("keypress", todoKeyHandler);
-// clearCompleted.addEventListener("keypress", clearItems);
-
-// dark theme - light theme
-function changeTheme() {
-	document.body.classList.toggle("light")
-}
-// have to use forEach because it is a nodeList
-
+let completedCount = 0;
+let nonCompletedCount = 0;
 
 // add a todo entry
 function addEntry(entry) {
-	const label = document.createElement("label");
-	label.setAttribute("class", "list-item");
-	label.setAttribute("draggable", "true");
 
+  console.log(entry)
+	const listItem = document.createElement("div");
+	listItem.classList.add("list-item");
+  
+	const label = document.createElement("label");
+	label.setAttribute("draggable", "true");
+  
 	const cb = document.createElement("input");
 	cb.setAttribute("class", "radio");
 	cb.setAttribute("name", "input-check");
 	cb.setAttribute("type", "checkbox");
-	cb.addEventListener("change", () => { cb.parentElement.classList.toggle("crossed"); });
-
+	cb.addEventListener("change", () => {
+	  cb.parentElement.classList.toggle("crossed");
+	  updateCounts();
+	});
+  
 	const cbspan = document.createElement("span");
 	cbspan.setAttribute("class", "checkmark");
-
+  
 	const deldiv = document.createElement("div");
 	deldiv.setAttribute("class", "delete");
-
+  
 	const delbtn = document.createElement("button");
 	delbtn.setAttribute("class", "delete-btn");
 	delbtn.setAttribute("type", "button");
 	delbtn.addEventListener("click", deleteItem);
 
-	deldiv.appendChild(delbtn);
-	label.appendChild(cb);
-	label.appendChild(cbspan);
-	label.appendChild(document.createTextNode(entry));
-	label.appendChild(deldiv);
+    deldiv.appendChild(delbtn);
+    label.appendChild(cb);
+    label.appendChild(cbspan);
+    label.appendChild(document.createTextNode(entry));
+    label.appendChild(deldiv);
+    
+    listItem.appendChild(label);
+    document.getElementById("list").appendChild(listItem);
 
-	document.getElementById("list").appendChild(label);
-}
+	nonCompletedCount++;
+	updateCounts();
+  }
+
 
 // handle enter key press on the todo list
 function todoKeyHandler(e) {
-	if (e.key !== "Enter")
-		return;
-	if (newTodo.value.length == 0) {
-		alert.textContent = "Please enter value";
-		setTimeout(function () {
-			alert.textContent = "";
-		}, 3000);
-		return;
-	}
-	addEntry(e.target.value);
-	newTodo.value = ''; // reset the new todo input to empty string 
+  console.log(e)
+  if (e.key !== "Enter") return;
+  if (newTodo.value.length === 0) {
+    alert.textContent = "Please enter value";
+    setTimeout(function () {
+      alert.textContent = "";
+    }, 3000);
+    return;
+  }
+  addEntry(newTodo.value);
+  newTodo.value = ''; // reset the new todo input to empty string 
 }
 
+input.addEventListener('keydown', function(e){
+	todoKeyHandler(e)
+  })
+
+
 // delete function 
-function deleteItem (e) {
-	e.target.parentNode.parentNode.remove() // this gets the parent element of the element you click, so; <div class="delete">. Then goes up one layer again to the <label> then removes the selected <label> Node with remove()
+function deleteItem(e) {
+  const parent = e.target.parentNode.parentNode;
+  if (parent.querySelector('input[type=checkbox]').checked) {
+    completedCount--;
+  } else {
+    nonCompletedCount--;
+  }
+  parent.remove();
+  updateCounts(); // call updateCounts()
 }
+
+
+// update completed and non-completed counts
+function updateCounts() {
+  completedCount = document.querySelectorAll('input[type=checkbox]:checked').length;
+  nonCompletedCount = document.querySelectorAll('input[type=checkbox]:not(:checked)').length;
+  console.log(`Completed entries: ${completedCount}`);
+  console.log(`Non-completed entries: ${nonCompletedCount}`);
+	itemsLeft.textContent = nonCompletedCount
+}
+
+
 
 // you can assign a function to "onload" rather than listening for DOMContentLoaded:
 onload = () => {
-	addEntry("Complete online JavaScript course");
-	addEntry("Jog around the park 3x");
-	addEntry("10 minutes meditation");
-	addEntry("Read for 1 hour");
-	addEntry("Pick up groceries");
+  addEntry("Complete online JavaScript course");
+  addEntry("Jog around the park 3x");
+  addEntry("10 minutes meditation");
+  addEntry("Read for 1 hour");
+  addEntry("Pick up groceries");
 }
 
-// items left number
-
-function countEntries() {
-	const entryLabels = document.querySelectorAll('.list-item');
-	return entryLabels.length;
-  }
-
-  const numEntries = countEntries();
-console.log(`Number of entries: ${numEntries}`);
-
-  
-
-// function showItemsLeft () {
-//     const count = list.filter(label => !label.crossed).length
-//     document.getElementById("items-left").innerHTML = count
-// 	console.log(count)
-// }
-// showItemsLeft()
-
-//     const itemsLeft = Array.from(todos).filter((t) => t.checked === false).length
-//     // // have to use Array.from because it is a nodeList
-//     // console.log(itemsLeft)
-//     // document.getElementById("items-left").innerHTML = itemsLeft
-// }
-// showItemsLeft();
-
-// function filterTodo(e) {
-// 	const entries = list.childNodes;
-// 	for(let i = 1; i<entries.length; i++ ){
-// 	switch (e.target.value) {
-// 	case "all":
-// 	entries[i].style.display = "flex";
-// 	break;
-// 	case "completed":
-// 	if (entries[i].classList.contains('crossed')) {
-// 	entries[i].style.display = "flex";
-// 	} else {
-// 	entries[i].style.display = "none";
-// 	}
-// 	break;
-// 	case "uncompleted":
-// 	if (!entries[i].classList.contains('crossed')) {
-// 	entries[i].style.display = "flex";
-// 	} else {
-// 	entries[i].style.display = "none";
-// 	}
-// 	break;
-// 	}}}
